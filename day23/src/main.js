7d3cce7febe1516fbc5f22967c2cd59e272732c9
Alvin <ajk.alvin@gmail.com>
2018-12-12 16:00:54 +0700

    add promise

--- day23/src/main.js
+++ day23/src/main.js
@@ -1,28 +1,37 @@
 // @flow
-
 import http from 'http';
-import fs from 'fs';
-import {join} from 'path';
 import Router from './Router';
 
 import serveHtml from './serveHtml';
 import serveFile from './serveFile';
-let server = http.createServer();
 
-server.on('error', (error) => {
-  console.log('ERROR SERVER : ', error);
-});
-let router = new Router();
-router.addRoute('/', ({request, response}) => {
-  serveHtml(response, 'index.html');
-});
-router.addRoute('/files/:fileName', ({request, response}, fileName) => {
-  serveFile(response, fileName);
-});
-server.on('request', (request, response) => {
-  router.handleRequest(request.url, {request, response});
+let routerPromise = new Promise((resolve, reject) => {
+  let router = new Router();
+  router.addRoute('/', ({request, response}) => {
+    serveHtml(response, 'index.html');
+  });
+  router.addRoute('/files/:fileName', ({request, response}, fileName) => {
+    serveFile(response, fileName);
+  });
+
+  resolve(router);
 });
 
-server.listen('9090', () => {
-  console.log('Server Started On 9090');
+let serverPromise = new Promise((resolve, reject) => {
+  let server = http.createServer();
+  server.on('error', (error) => {
+    console.log(error);
+  });
+
+  server.listen('9090', () => {
+    console.log('Server Started On 9090');
+    resolve(server);
+  });
+});
+
+Promise.all([routerPromise, serverPromise]).then(([router, server]) => {
+  server.on('request', (request, response) => {
+    router.handleRequest(request.url, {request, response});
+  });
+  console.log('All done');
 });
