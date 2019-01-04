6f9063848a852c964e467cdb87c76129006c6c9b
Alvin <ajk.alvin@gmail.com>
2019-01-04 10:46:54 +0700

    encoding task + bonus task

---
+++ day6regex/encodingtask.js
@@ -0,0 +1,25 @@
+function hexify(string) {
+  let result = '';
+  let replaceWith = '?'.charCodeAt(0).toString(16);
+  for (let i = 0; i < string.length; i++) {
+    let code = string.charCodeAt(i);
+    if (code > 256) {
+      result = result + replaceWith;
+    } else {
+      result = result + code.toString(16);
+    }
+  }
+  return result;
+}
+function bonusTask(string) {
+  let buffer = Buffer.from(string, 'hex');
+  return buffer.toString('utf8');
+}
+
+let test = hexify('hello world 你好世界');
+console.log(test);
+let bonus = bonusTask(test);
+console.log(bonus);
+
+//ååßß
+//ԬԱ
